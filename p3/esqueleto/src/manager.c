ea486290cdf47ab9c0f679899de0483aaedc8397
RaulJDlCRUZ <Raul.Jimenez8@alu.uclm.es>
2023-04-19 13:59:11 +0200

    consideraciones manager

--- p3/esqueleto/src/manager.c
+++ p3/esqueleto/src/manager.c
@@ -11,17 +11,17 @@
 #include <mqueue.h>
 #include <definitions.h>
 
-void crear_buzones();
-void instalar_manejador_senhal();
-void manejador_senhal(int sign);
-void iniciar_tabla_procesos(int n_procesos_telefono, int n_procesos_linea);
-void crear_procesos(int numTelefonos, int numLineas);
-void lanzar_proceso_telefono(const int indice_tabla);
-void lanzar_proceso_linea(const int indice_tabla);
-void esperar_procesos();
-void terminar_procesos(void);
-void terminar_procesos_especificos(struct TProcess_t *process_table, int process_num);
-void liberar_recursos();
+void crear_buzones();                                                                   //? Fijarse un poco en filosofos comensales T3
+void instalar_manejador_senhal();                                                       //! Copia pega 2
+void manejador_senhal(int sign);                                                        //! Copia pega 2
+void iniciar_tabla_procesos(int n_procesos_telefono, int n_procesos_linea);             //! Copia pega 2 o memset (mirar filosofos comensales T3)
+void crear_procesos(int numTelefonos, int numLineas);                                   //! Copia pega 2
+void lanzar_proceso_telefono(const int indice_tabla);                                   //? Fijarse en comensales T3 y tratar de hacer una mezcla
+void lanzar_proceso_linea(const int indice_tabla);                                      //? Fijarse en comensales T3 y tratar de hacer una mezcla
+void esperar_procesos();                                                                //! Copia pega 2, total, no va a hacer falta
+void terminar_procesos(void);                                                           //! Copia pega 2
+void terminar_procesos_especificos(struct TProcess_t *process_table, int process_num);  //! Copia pega 2
+void liberar_recursos();                                                                //? Fijarse en comensales T3 y tratar de hacer una mezcla
 
 int g_telefonosProcesses = 0;
 int g_lineasProcesses = 0;
@@ -29,7 +29,6 @@ struct TProcess_t *g_process_telefonos_table;
 struct TProcess_t *g_process_lineas_table;
 mqd_t qHandlerLlamadas;
 mqd_t qHandlerLineas[NUMLINEAS];
-
 
 int main(int argc, char *argv[])
 {
@@ -40,20 +39,19 @@ int main(int argc, char *argv[])
 
     // Manejador de Ctrl-C
     instalar_manejador_senhal();
- 
+
     // Crea Tabla para almacenar los pids de los procesos
-    iniciar_tabla_procesos(NUMTELEFONOS,NUMLINEAS);
+    iniciar_tabla_procesos(NUMTELEFONOS, NUMLINEAS);
 
     // Tenemos todo
     // Lanzamos los procesos
-    crear_procesos(NUMTELEFONOS,NUMLINEAS);
-  
+    crear_procesos(NUMTELEFONOS, NUMLINEAS);
+
     // Esperamos a que finalicen las lineas
     esperar_procesos();
 
     // Matamos los telefonos y cualquier otro proceso restante
     terminar_procesos();
-
 
     // Finalizamos Manager
     printf("\n[MANAGER] Terminacion del programa (todos los procesos terminados).\n");
@@ -62,4 +60,4 @@ int main(int argc, char *argv[])
     return EXIT_SUCCESS;
 }
 
-//TODO
+// TODO
